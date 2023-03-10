330ffaa93c7c86ae88f7f378f5c92431bd3e6e8a
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-10 17:33:15 +0530

    Fetched Search Data

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '4078ad3a5cmshb3424c404cf42cbp11b3e7jsna53b8adcbdb2',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
