f17eb00846e39aceab971a9b9de3407b6f856888
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-11 11:16:15 +0530

    Exercise Component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import {React, useEffect , useState} from 'react'
+import Pagination from '@mui/material/Pagination'
+import {Box, Stack, Typography} from '@mui/material' ;
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
+
+const Exercises = ({bodyPart, exercises, setExercises}) => {
+  // const exercises=props.exercises;
+  console.log(exercises);
+  
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+      sx={{mt: {lg: '110px'}}}
+      mt="50px"
+      p="20px"
+    >
+       <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">
+        Showing Results
+        </Typography>
+        <Stack direction="row" sx={{ gap: {lg: '110px' , xs: '50px'}}} 
+        flexWrap="wrap" justifyContent="center"> 
+        {exercises.map((exercise,index)=>(
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+        </Stack>
+    </Box>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
-import React, {useState} from 'react';
-import {Box} from '@mui/material';
-import Exercises from '../components/Exercises'
-import HeroBanner from '../components/HeroBanner'
-import SearchExercises from '../components/SearchExercises'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  const[exercises, setExercises] =  useState([]);
-  const [bodyPart,setBodyPart]= useState('all');
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises  bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises}/>
-      <Exercises  bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises}/>
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
