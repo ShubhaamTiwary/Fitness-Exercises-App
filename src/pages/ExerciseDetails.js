672bf04fd6d607ff849e7072d48cdd87cc474260
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-12 19:15:51 +0530

    Videos + Exercises recommendation done

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, YoutubeOptions } from '../utils/fetchData';
 import Detail from '../components/Details';
 import ExerciseVideos from '../components/ExerciseVideo';
 import SimilarExercises from '../components/SimilarExercises';
@@ -24,8 +24,8 @@ const ExerciseDetail = () => {
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
 
-      // const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
-      // setExerciseVideos(exerciseVideosData.contents);
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, YoutubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
 
       const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
       setTargetMuscleExercises(targetMuscleExercisesData);
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,14 @@ export const exerciseOptions = {
   },
 };
 
+export const YoutubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '4078ad3a5cmshb3424c404cf42cbp11b3e7jsna53b8adcbdb2',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
