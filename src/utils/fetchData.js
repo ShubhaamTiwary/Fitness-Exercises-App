645c3d6092b8926df106aaa12e3ab0eb37227972
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-08-10 04:34:59 +0530

    API Updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,20 @@
 export const exerciseOptions = {
   method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': 'ce4af4f226mshdf0f1a5f68c5df5p135ea3jsne57d420481f8',
+    'X-RapidAPI-Key': '7fb4b0b00dmsha1d6c6b36ee2687p1c06d7jsnc25e8b0f6172',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
 
 export const YoutubeOptions = {
   method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+  params: {
+    id: 'UCE_M8A5yxnLfW0KghEeajjw'
+  },
   headers: {
-    'X-RapidAPI-Key': 'ce4af4f226mshdf0f1a5f68c5df5p135ea3jsne57d420481f8',
+    'X-RapidAPI-Key': '7fb4b0b00dmsha1d6c6b36ee2687p1c06d7jsnc25e8b0f6172',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
