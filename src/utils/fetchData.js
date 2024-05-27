01f4e4f13c4cb286a12fdc71630d80a3838ff48b
ShubhaamTiwary <shubhaamtiwary@gmail.com>
2024-05-27 09:22:42 +0530

    chore: Update API keys in fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': '170644603fmsh830757336261629p1db16ejsn413209fce587',
+    'X-RapidAPI-Key': '7fb4b0b00dmsha1d6c6b36ee2687p1c06d7jsnc25e8b0f6172',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -14,7 +14,7 @@ export const YoutubeOptions = {
     id: 'UCE_M8A5yxnLfW0KghEeajjw'
   },
   headers: {
-    'X-RapidAPI-Key': '170644603fmsh830757336261629p1db16ejsn413209fce587',
+    'X-RapidAPI-Key': '7fb4b0b00dmsha1d6c6b36ee2687p1c06d7jsnc25e8b0f6172',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
